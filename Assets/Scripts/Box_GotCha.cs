9d477f12013fd4c080c9d9240c45562a5aa37a3a
crome7717 <153704575+crome7717@users.noreply.github.com>
2023-12-19 05:03:28 +0900

    12.19 home
    12.19 home

--- Assets/Scripts/Box_GotCha.cs
+++ Assets/Scripts/Box_GotCha.cs
@@ -39,6 +39,7 @@ public class Box_GotCha : MonoBehaviour
                 }
                         Debug.Log("골드:"+gold+" 에메랄드:"+emerald+" 루비:"+ruby);
         */
+        
         int grade1 = 0;
         int grade2 = 0;
         int grade3 = 0;
@@ -46,59 +47,56 @@ public class Box_GotCha : MonoBehaviour
         int grade5 = 0;
 
         int randompersent;
-        int lastpan= 1;
-        
-        for (int i = 1; i < 31; i++) 
+        int lastpang = 1;
+
+        for (lastpang = 1; lastpang <= 30; lastpang++)
         {
             randompersent = Random.Range(1, 101);
-            if (lastpan >= 30)
+            if (lastpang >= 29 && grade5 == 0)
             {
+                if (lastpang == 30 && grade5 == 0)
+                {   
+                grade5 = 1;
+                Debug.Log(lastpang);
+                Debug.Log("불쌍해서 하나 드렸습니다.");
 
+                }
             }
             else if (randompersent < 21)
             {
                 grade1 += Random.Range(20, 31);
-                lastpan++;
+                //lastpang++;
+                Debug.Log(lastpang);
             }
             else if (randompersent > 21 && randompersent < 51)
             {
                 grade2 += Random.Range(15, 26);
-                lastpan++;
+                //lastpang++;
+                Debug.Log(lastpang);
             }
             else if (randompersent > 51 && randompersent < 91)
             {
                 grade3 += Random.Range(10, 21);
-                lastpan++;
+                //lastpang++;
+                Debug.Log(lastpang);
             }
             else if (randompersent > 91 && randompersent < 99)
             {
                 grade4 += Random.Range(5, 11);
-                lastpan++;
+                //lastpang++;
+                Debug.Log(lastpang);
             }
-            else if     (randompersent > 98 && randompersent < 101)
+            else if (randompersent > 98 && randompersent < 101)
             {
                 grade5 += Random.Range(1, 6);
-                lastpan = 1;
+                //lastpang++;
+                Debug.Log(lastpang);
             }
-
-                
             
-                
-        }
-        Debug.Log("1등급은 "+ grade1 + ", "+ "2등급은 "+grade2+", "+"3등급은 "+grade3+", "+"4등급은 "+grade4+", "+"5등급은 "+grade5 +" 입니다." );
-        if (grade5 == 0)
-        {
-            Debug.Log("30회 동안 5등급을 얻지 못해 1개를 확정적으로 드립니다.");
-            grade5 += 1;
-        Debug.Log("1등급은 " + grade1 + ", " + "2등급은 " + grade2 + ", " + "3등급은 " + grade3 + ", " + "4등급은 " + grade4 + ", " + "5등급은 " + grade5 + " 입니다.");
-        }
-        else
-        {
-            Debug.Log("뽑기를 종료 합니다.");
+            Debug.Log(lastpang + "이것은 lastpang 변수 입니다");
         }   
+        Debug.Log("1등급은 "+ grade1 + ", "+ "2등급은 "+grade2+", "+"3등급은 "+grade3+", "+"4등급은 "+grade4+", "+"5등급은 "+grade5 +" 입니다." );
+
+            Debug.Log("뽑기를 종료 합니다.");
     }
 }
-
-
-
-
