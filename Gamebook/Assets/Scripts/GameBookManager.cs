e8c90b4e184a9affaa3f27f9713da44029473841
crome7717 <153704575+crome7717@users.noreply.github.com>
2023-12-22 06:37:57 +0900

    12.22 home
    12.22 home
    게임북 기초 UI 작성
    씬 변경 추가, 게임 종료 버튼 활성화

---
+++ Gamebook/Assets/Scripts/GameBookManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameBookManager : MonoBehaviour
+{ 
+
+    public void Totitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void Start() 
+    {
+ 
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
